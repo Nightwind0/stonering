f36c997aee955b47133e2edb52abde5a13228ac2
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2005-02-20 19:32:14 +0000

    Splash screen, and dtd fixes.

--- sr2/src/Application.cpp
+++ sr2/src/Application.cpp
@@ -1,6 +1,7 @@
 #include <ClanLib/display.h>
 #include <ClanLib/core.h>
 #include <ClanLib/gl.h>
+#include <iostream>
 
 #include "Application.h"
 
@@ -43,16 +44,61 @@ int Application::main(int argc, char ** argv)
 	
 	try
 	{
+
 		setupClanLib();
-		CL_DisplayWindow window ("Stone Ring", WINDOW_WIDTH, WINDOW_HEIGHT);
 		
+		mpResources = new CL_ResourceManager ( "Media/resources.xml" );
+
+		std::string name = CL_String::load("Configuration/name", mpResources);
+		mpWindow  = new CL_DisplayWindow(name, WINDOW_WIDTH, WINDOW_HEIGHT);
+		
+		CL_Display::clear();
+		
+		showRechargeableOnionSplash();
+		showIntro();
+
+		
+
 		
 		teardownClanLib();
 	}
-	catch(...)
+	catch(CL_Error error)
 	{
+		std::cerr << "Exception Caught!!" << std::endl;
+		std::cerr << error.message.c_str() << std::endl;
 	}
 	
+}
+
+void Application::showRechargeableOnionSplash()
+{
+}
+
+void Application::showIntro()
+{
+
+
+	CL_Surface splash("Configuration/splash", mpResources);
+	CL_Surface background("Configuration/splashbg", mpResources);
+
+	CL_GraphicContext *gc = mpWindow->get_gc();
+
+	int displayX = (WINDOW_WIDTH - splash.get_width()) / 2;
+	int displayY = (WINDOW_HEIGHT - splash.get_height()) / 2;
+
+
+
+	while(!CL_Keyboard::get_keycode(CL_KEY_ESCAPE) && !CL_Keyboard::get_keycode(CL_KEY_ENTER) && !CL_Keyboard::get_keycode( CL_KEY_SPACE))
+	{
+
+		background.draw(0,0);
+		splash.draw(displayX,displayY);
+
+		CL_Display::flip();
+		CL_System::keep_alive();
+	}
+	
+
 }
 
 Application app;
