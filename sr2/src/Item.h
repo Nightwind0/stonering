4d68f3e501d3afa71f825cdeb5b030b279ab052b
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2005-02-22 04:01:23 +0000

    Forgot to check in item.h

---
+++ sr2/src/Item.h
@@ -0,0 +1,24 @@
+#ifndef SR_ITEM_H
+#define SR_ITEM_H
+
+
+namespace StoneRing{
+
+class Item
+{
+ public:
+  Item();
+  ~Item();
+
+  enum eItemType { ITEM, WEAPON, ARMOR, RUNE, SPECIAL };
+
+
+ private:
+};
+
+};
+
+
+
+
+#endif
