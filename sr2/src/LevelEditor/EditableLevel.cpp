76ecd4c12c8478052ed83ef04da17b8f3780a2de
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2005-03-01 06:20:36 +0000

    Editable Level should work okay for the most part, and I added one to editor.

--- sr2/src/LevelEditor/EditableLevel.cpp
+++ sr2/src/LevelEditor/EditableLevel.cpp
@@ -31,6 +31,21 @@ EditableTilemap::~EditableTilemap()
 }
 
 
+EditableSpriteRef::EditableSpriteRef( const std::string &ref, StoneRing::SpriteRef::eDirection dir )
+{
+    mRef = ref;
+    meDirection = dir;
+}
+EditableSpriteRef::EditableSpriteRef(CL_DomElement * pElement ):SpriteRef(pElement)
+{
+}
+
+EditableSpriteRef::~EditableSpriteRef()
+{
+}
+
+
+
 EditableTile::EditableTile()
 {
 }
@@ -62,29 +77,85 @@ void EditableTile::setZOrder(int z)
 // Has to have one of these if it's new
 void EditableTile::setTilemap( const std::string &mapname, uint mapX, uint mapY)
 {
-    
+    // TODO: Factory method...
+    mGraphic.asTilemap = new EditableTilemap( mapname,mapX,mapY );   
 }
 
 void EditableTile::setSpriteRef ( const std::string &spriteRef, StoneRing::SpriteRef::eDirection direction )
 {
+    GraphicsManager * GM = GraphicsManager::getInstance();
+    mGraphic.asSpriteRef = new EditableSpriteRef (spriteRef, direction   );
 }
 
 
 void EditableTile::setIsFloater()
 {
+    cFlags |= FLOATER;
 }
 
 void EditableTile::setIsHot()
 {
+    cFlags |= HOT;
 }
 
 void EditableTile::setDirectionBlock (int dirBlock )
 {
+    if(dirBlock & StoneRing::DIR_NORTH)
+	cFlags |= BLK_NORTH;
+    if(dirBlock & StoneRing::DIR_SOUTH)
+	cFlags |= DIR_SOUTH;
+    if(dirBlock & StoneRing::DIR_WEST)
+	cFlags |= DIR_WEST;
+    if(dirBlock & StoneRing::DIR_EAST)
+	cFlags |= DIR_EAST;
 }
     
 
 
+void EditableLevel::loadTile ( CL_DomElement * tileElement)
+{
 
+    CL_Point point;
+
+
+
+    // TODO: We need a factory which we get from IApplication,
+    // and then we call pFactory->createTile ( tileElement );
+    // and it could give us back a Tile, or an EditableTile
+    // Then we wouldn't have to overload this method at all
+
+    EditableTile * tile = new EditableTile ( tileElement );
+
+    point.x = tile->getX();
+    point.y = tile->getY();
+	
+    if( tile->isFloater() )
+    {
+	std::cout << "Placing floater at: " << point.x << ',' << point.y << std::endl;
+
+	mFloaterMap[ point ].push_back ( tile );
+
+#ifndef _MSC_VER
+	mFloaterMap[ point ].sort( &tileSortCriterion );
+#else
+	mFloaterMap[ point ].sort( std::greater<Tile*>() );
+#endif
+    }
+    else
+    {
+
+
+		
+	mTileMap[ point.x ][point.y].push_back ( tile );
+
+	// Sort by ZOrder, so that they display correctly
+#ifndef _MSC_VER
+	mTileMap[ point.x ][point.y].sort( &tileSortCriterion );
+#else
+	mTileMap[ point.x ][point.y].sort(std::greater<Tile*>() );
+#endif
+    }
+}
 
 EditableLevel::EditableLevel(const std::string &name,CL_ResourceManager * pResources):Level(name,pResources)
 {
@@ -101,10 +172,29 @@ void EditableLevel::drawMappableObjects(const CL_Rect &src, const CL_Rect &dst, 
 		
 void EditableLevel::addTile ( Tile * pTile )
 {
+    if( pTile->isFloater())
+    {
+	mFloaterMap[ CL_Point(pTile->getX(),pTile->getY()) ].push_back(pTile);
+    }
+    else
+    {
+	mTileMap[ pTile->getX() ][ pTile->getY()].push_back ( pTile );	
+    }
 }
 
 void EditableLevel::removeTile ( Tile * pTile )
 {
+    if(pTile->isFloater())
+    {
+	if(mFloaterMap.count( CL_Point(pTile->getX(), pTile->getY() ) ))
+	{
+	    mFloaterMap[CL_Point(pTile->getX(),pTile->getY())].remove( pTile );
+	}
+    }
+    else
+    {
+	mTileMap[pTile->getX()][pTile->getY()].remove (pTile );
+    }
 }
  
 std::list<Tile*> EditableLevel::getTilesAt(uint levelX, uint levelY) const
