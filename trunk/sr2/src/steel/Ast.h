f4527da8a3d79fe97a3b8bdf49bc126a42049798
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2006-12-27 21:23:46 +0000

    Various fixes and first appearance of the Ast. Also, switched the scanner to read from a provided buffer, rather than from cin

---
+++ trunk/sr2/src/steel/Ast.h
@@ -0,0 +1,12 @@
+#ifndef STEEL_AST_H
+#define STEEL_AST_H
+
+class AstBase
+{
+public:
+    AstBase();
+    virtual ~AstBase();
+private:
+};
+
+#endif
--- trunk/sr2/src/steel/test_parser.cpp
+++ trunk/sr2/src/steel/test_parser.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+//#include <ifstream>
 #include "SteelParser.h"
 
 int main()
 {
     SteelParser parser;
+    std::string script;
+
+    while(!std::cin.eof())
+    {
+	script += std::cin.get();
+    }
+
+    std::cout << "STEEL SCRIPT: [[" << std::endl;
+    std::cout << script << "]]" << std::endl;
+
 
     parser.SetDebugSpewLevel(2);
+    parser.setBuffer( script.c_str() );
     if(parser.Parse() != SteelParser::PRC_SUCCESS)
     {
 	std::cout << "Parse error unknown." << std::endl;
