99486980c8fe238059f2863f623778dd5a9b2451
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2007-04-16 01:18:40 +0000

    Consting things and making editor elements derived from WriteableElement

--- sr2/src/LevelEditor/EditorElements.h
+++ sr2/src/LevelEditor/EditorElements.h
@@ -138,88 +138,88 @@ class AttributeEnhancer : public StoneRing::AttributeEnhancer, public WriteableE
     virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class BattleMenu : public StoneRing::BattleMenu
+class BattleMenu : public StoneRing::BattleMenu, public WriteableElement
 {
  public:
 	BattleMenu();
 	virtual ~BattleMenu();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class BattleMenuOption : public StoneRing::BattleMenuOption
+class BattleMenuOption : public StoneRing::BattleMenuOption, public WriteableElement
 {
  public:
 	BattleMenuOption();
 	virtual ~BattleMenuOption();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class CharacterClass : public StoneRing::CharacterClass
+class CharacterClass : public StoneRing::CharacterClass, public WriteableElement
 {
  public:
 	CharacterClass();
 	virtual ~CharacterClass();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class CharacterDefinition : public StoneRing::CharacterDefinition
+class CharacterDefinition : public StoneRing::CharacterDefinition, public WriteableElement
 {
  public:
 	CharacterDefinition();
 	virtual ~CharacterDefinition();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class ConditionScript : public StoneRing::ScriptElement
+class ConditionScript : public StoneRing::ScriptElement, public WriteableElement
 {
  public:
 	ConditionScript();
 	virtual ~ConditionScript();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class DirectionBlock : public StoneRing::DirectionBlock
+class DirectionBlock : public StoneRing::DirectionBlock, public WriteableElement
 {
  public:
 	DirectionBlock();
     DirectionBlock(int);
 	virtual ~DirectionBlock();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Event : public StoneRing::Event
+class Event : public StoneRing::Event, public WriteableElement
 {
  public:
 	Event();
 	virtual ~Event();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
     void setScript(StoneRing::ScriptElement *pScript){ mpScript = pScript; }
  private:
 };
-class IconRef : public StoneRing::IconRef
+class IconRef : public StoneRing::IconRef, public WriteableElement
 {
  public:
 	IconRef();
 	virtual ~IconRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class ItemRef : public StoneRing::ItemRef
+class ItemRef : public StoneRing::ItemRef, public WriteableElement
 {
  public:
 	ItemRef();
 	virtual ~ItemRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Level : public StoneRing::Level
+class Level : public StoneRing::Level, public WriteableElement
 {
  public:
 	Level();
 	virtual ~Level();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
     virtual void drawMappableObjects(const CL_Rect &src, const CL_Rect &dst, CL_GraphicContext *pGC);
-    std::list<StoneRing::Tile*> getTilesAt(uint levelX, uint levelY) const;
+    std::list<StoneRing::Tile*> getTilesAt(uint levelX, uint levelY)const;
     // Operates on ALL tiles at a location. For finer control, one must operate on the tiles individually.
     // bOn of true turns the direction block on for the specified direction,
     // false will turn it off.
@@ -237,164 +237,164 @@ class Level : public StoneRing::Level
     void setMusic(const std::string &music);
  private:
 };
-class LevelHeader : public StoneRing::LevelHeader
+class LevelHeader : public StoneRing::LevelHeader, public WriteableElement
 {
  public:
 	LevelHeader();
 	virtual ~LevelHeader();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class MagicDamageCategory : public StoneRing::MagicDamageCategory
+class MagicDamageCategory : public StoneRing::MagicDamageCategory, public WriteableElement
 {
  public:
 	MagicDamageCategory();
 	virtual ~MagicDamageCategory();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class MagicResistance  : public StoneRing::MagicResistance 
+class MagicResistance  : public StoneRing::MagicResistance , public WriteableElement
 {
  public:
 	MagicResistance();
 	virtual ~MagicResistance();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class MappableObject : public StoneRing::MappableObject
+class MappableObject : public StoneRing::MappableObject, public WriteableElement
 {
  public:
 	MappableObject();
 	virtual ~MappableObject();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class MappableObjects : public StoneRing::MappableObjects
+class MappableObjects : public StoneRing::MappableObjects, public WriteableElement
 {
  public:
 	MappableObjects();
 	virtual ~MappableObjects();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Movement : public StoneRing::Movement
+class Movement : public StoneRing::Movement, public WriteableElement
 {
  public:
 	Movement();
 	virtual ~Movement();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class NamedItemElement : public StoneRing::NamedItemElement
+class NamedItemElement : public StoneRing::NamedItemElement, public WriteableElement
 {
  public:
 	NamedItemElement();
 	virtual ~NamedItemElement();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class NamedItemRef : public StoneRing::NamedItemRef
+class NamedItemRef : public StoneRing::NamedItemRef, public WriteableElement
 {
  public:
 	NamedItemRef();
 	virtual ~NamedItemRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnEquip : public StoneRing::OnEquip
+class OnEquip : public StoneRing::OnEquip, public WriteableElement
 {
  public:
 	OnEquip();
 	virtual ~OnEquip();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnUnequip : public StoneRing::OnUnequip
+class OnUnequip : public StoneRing::OnUnequip, public WriteableElement
 {
  public:
 	OnUnequip();
 	virtual ~OnUnequip();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnRound : public StoneRing::OnRound
+class OnRound : public StoneRing::OnRound, public WriteableElement
 {
  public:
 	OnRound();
 	virtual ~OnRound();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnStep : public StoneRing::OnStep
+class OnStep : public StoneRing::OnStep, public WriteableElement
 {
  public:
 	OnStep();
 	virtual ~OnStep();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnCountdown : public StoneRing::OnCountdown
+class OnCountdown : public StoneRing::OnCountdown, public WriteableElement
 {
  public:
 	OnCountdown();
 	virtual ~OnCountdown();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnInvoke : public StoneRing::OnInvoke
+class OnInvoke : public StoneRing::OnInvoke, public WriteableElement
 {
  public:
 	OnInvoke();
 	virtual ~OnInvoke();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class OnRemove : public StoneRing::OnRemove
+class OnRemove : public StoneRing::OnRemove, public WriteableElement
 {
  public:
 	OnRemove();
 	virtual ~OnRemove();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Par : public StoneRing::Par
+class Par : public StoneRing::Par, public WriteableElement
 {
  public:
 	Par();
 	virtual ~Par();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class RegularItem : public StoneRing::RegularItem
+class RegularItem : public StoneRing::RegularItem, public WriteableElement
 {
  public:
 	RegularItem();
 	virtual ~RegularItem();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Rune : public StoneRing::Rune
+class Rune : public StoneRing::Rune, public WriteableElement
 {
  public:
 	Rune();
 	virtual ~Rune();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class RuneType : public StoneRing::RuneType
+class RuneType : public StoneRing::RuneType, public WriteableElement
 {
  public:
 	RuneType();
 	virtual ~RuneType();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class ScriptElement : public StoneRing::ScriptElement
+class ScriptElement : public StoneRing::ScriptElement, public WriteableElement
 {
  public:
 	ScriptElement();
 	virtual ~ScriptElement();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
     void setScript(const std::string &);
 
     // May throw SteelException. Please catch and present
@@ -403,94 +403,94 @@ private:
     virtual void handleText(const std::string &);
     std::string mScript;
 };
-class Skill : public StoneRing::Skill
+class Skill : public StoneRing::Skill, public WriteableElement
 {
  public:
 	Skill();
 	virtual ~Skill();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class SkillRef : public StoneRing::SkillRef
+class SkillRef : public StoneRing::SkillRef, public WriteableElement
 {
  public:
 	SkillRef();
 	virtual ~SkillRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class SpecialItem : public StoneRing::SpecialItem
+class SpecialItem : public StoneRing::SpecialItem, public WriteableElement
 {
  public:
 	SpecialItem();
 	virtual ~SpecialItem();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Spell : public StoneRing::Spell
+class Spell : public StoneRing::Spell, public WriteableElement
 {
  public:
 	Spell();
 	virtual ~Spell();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class SpellRef : public StoneRing::SpellRef
+class SpellRef : public StoneRing::SpellRef, public WriteableElement
 {
  public:
 	SpellRef();
 	virtual ~SpellRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class SpriteRef : public StoneRing::SpriteRef
+class SpriteRef : public StoneRing::SpriteRef, public WriteableElement
 {
  public:
 	SpriteRef();
 	virtual ~SpriteRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
     void setSpriteRef( const std::string &ref);
     void setType( StoneRing::SpriteRef::eType dir);
  private:
 };
-class StatIncrease : public StoneRing::StatIncrease
+class StatIncrease : public StoneRing::StatIncrease, public WriteableElement
 {
  public:
 	StatIncrease();
 	virtual ~StatIncrease();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class StatusEffect : public StoneRing::StatusEffect
+class StatusEffect : public StoneRing::StatusEffect, public WriteableElement
 {
  public:
 	StatusEffect();
 	virtual ~StatusEffect();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class StatusEffectModifier : public StoneRing::StatusEffectModifier
+class StatusEffectModifier : public StoneRing::StatusEffectModifier, public WriteableElement
 {
  public:
 	StatusEffectModifier();
 	virtual ~StatusEffectModifier();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class SystemItem : public StoneRing::SystemItem
+class SystemItem : public StoneRing::SystemItem, public WriteableElement
 {
  public:
 	SystemItem();
 	virtual ~SystemItem();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Tile : public StoneRing::Tile
+class Tile : public StoneRing::Tile, public WriteableElement
 {
  public:
 	Tile();
 	virtual ~Tile();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
     void setLevelX(int x);
     void setLevelY(int y);
     void setZOrder(int z);
@@ -509,111 +509,111 @@ class Tile : public StoneRing::Tile
     void setNotHot();
  private:
 };
-class Tiles : public StoneRing::Tiles
+class Tiles : public StoneRing::Tiles, public WriteableElement
 {
  public:
 	Tiles();
 	virtual ~Tiles();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class Tilemap : public StoneRing::Tilemap
+class Tilemap : public StoneRing::Tilemap, public WriteableElement
 {
  public:
 	Tilemap();
 	virtual ~Tilemap();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
     void setMapName(const std::string & mapname);
     void setMapX(int x);
     void setMapY(int y);
  private:
 };
-class UniqueArmor : public StoneRing::UniqueArmor
+class UniqueArmor : public StoneRing::UniqueArmor, public WriteableElement
 {
  public:
 	UniqueArmor();
 	virtual ~UniqueArmor();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class UniqueWeapon : public StoneRing::UniqueWeapon
+class UniqueWeapon : public StoneRing::UniqueWeapon, public WriteableElement
 {
  public:
 	UniqueWeapon();
 	virtual ~UniqueWeapon();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponClass : public StoneRing::WeaponClass
+class WeaponClass : public StoneRing::WeaponClass, public WriteableElement
 {
  public:
 	WeaponClass();
 	virtual ~WeaponClass();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponClassRef : public StoneRing::WeaponClassRef
+class WeaponClassRef : public StoneRing::WeaponClassRef, public WriteableElement
 {
  public:
 	WeaponClassRef();
 	virtual ~WeaponClassRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponDamageCategory : public StoneRing::WeaponDamageCategory
+class WeaponDamageCategory : public StoneRing::WeaponDamageCategory, public WriteableElement
 {
  public:
 	WeaponDamageCategory();
 	virtual ~WeaponDamageCategory();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponEnhancer : public StoneRing::WeaponEnhancer
+class WeaponEnhancer : public StoneRing::WeaponEnhancer, public WriteableElement
 {
  public:
 	WeaponEnhancer();
 	virtual ~WeaponEnhancer();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponRef : public StoneRing::WeaponRef
+class WeaponRef : public StoneRing::WeaponRef, public WriteableElement
 {
  public:
 	WeaponRef();
 	virtual ~WeaponRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponType : public StoneRing::WeaponType
+class WeaponType : public StoneRing::WeaponType, public WriteableElement
 {
  public:
 	WeaponType();
 	virtual ~WeaponType();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponTypeExclusionList : public StoneRing::WeaponTypeExclusionList
+class WeaponTypeExclusionList : public StoneRing::WeaponTypeExclusionList, public WriteableElement
 {
  public:
 	WeaponTypeExclusionList();
 	virtual ~WeaponTypeExclusionList();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponTypeRef : public StoneRing::WeaponTypeRef
+class WeaponTypeRef : public StoneRing::WeaponTypeRef, public WriteableElement
 {
  public:
 	WeaponTypeRef();
 	virtual ~WeaponTypeRef();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
-class WeaponTypeSprite : public StoneRing::WeaponTypeSprite
+class WeaponTypeSprite : public StoneRing::WeaponTypeSprite, public WriteableElement
 {
  public:
 	WeaponTypeSprite();
 	virtual ~WeaponTypeSprite();
-    virtual CL_DomElement createDomElement(CL_DomDocument &doc);
+    virtual CL_DomElement createDomElement(CL_DomDocument &doc)const;
  private:
 };
 
