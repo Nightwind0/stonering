7082b0700b40be26c326d4b0ea31f5912939c60e
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2007-11-21 00:16:56 +0000

    Added some parameters to the steel executable so that you can just check if it parses, and you can cause it to print out the script if you want to see what the tree looks like, kind of.

--- trunk/steel/src/steel.cpp
+++ trunk/steel/src/steel.cpp
@@ -2,19 +2,46 @@
 #include "SteelInterpreter.h"
 #include "SteelException.h"
 
-int main()
+int main(int argc, char * argv[])
 {
     SteelInterpreter interpreter;
 
     std::string script;
 
+    bool bPrint=false;
+    bool bRun=true;
+
+    for(int i=0;i<argc;i++)
+    {
+        std::string arg = argv[i];
+
+        if(arg == "--print" ||
+           arg == "-p")
+        {
+            bPrint = true;
+        }
+        else if(arg == "--no-exec" ||
+                arg == "-n")
+        {
+            bRun = false;
+        }
+        
+    }
+
+
     while(!std::cin.eof())
     {
-    script += std::cin.get();
+        script += std::cin.get();
     }
 
     try{
-        interpreter.run("STDIO", script);
+        AstScript * pScript = interpreter.prebuildAst("<STDIN>",script);
+
+        if(bPrint)
+            pScript->print(std::cout);
+
+        if(bRun)
+            interpreter.runAst(pScript);
     }
     catch(SteelException ex)
     {
