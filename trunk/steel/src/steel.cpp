60941a8e5c5671633c0a358221daa03b5c7c87bf
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2011-01-11 23:51:26 +0000

    Improvements to executable, and build

--- trunk/steel/src/steel.cpp
+++ trunk/steel/src/steel.cpp
@@ -1,6 +1,26 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include "SteelInterpreter.h"
 #include "SteelException.h"
+
+
+void usage(){
+  std::cout << "steel [options] <filename>" << std::endl << 
+    "Options:" << std::endl <<
+    "-p,--print: ask the script to print itself out after being built" << std::endl <<
+    "-n,--no-exec: don't execute the script, just build it" << std::endl <<
+    "-d,--debug: show scanner and parser debug spew" << std::endl << 
+    "--no-scan-debug: use with -d, show only parser debug spew" << std::endl;
+}
+
+
+bool read_script(std::istream& stream,std::string& into)
+{
+  into.assign( (std::istreambuf_iterator<char>(stream) ),
+                       (std::istreambuf_iterator<char>()    ) );
+}
+
 
 int main(int argc, char * argv[])
 {
@@ -12,8 +32,10 @@ int main(int argc, char * argv[])
     bool bRun=true;
     bool bDebug = false;
     bool bScanDebug = false;
+    bool bCin = false;
+    std::string filename;
 
-    for(int i=0;i<argc;i++)
+    for(int i=1;i<argc;i++)
     {
         std::string arg = argv[i];
 
@@ -35,19 +57,39 @@ int main(int argc, char * argv[])
         {
             bDebug = true;
 	    bScanDebug = true;
-        }
-
-        
+        }else if(arg == "-" && i == (argc-1)){
+	  bCin = true;
+	}else {
+	  if(!filename.size()){
+	    filename = arg;
+	  }else{
+	    usage();
+	    return -1;
+	  }
+	}
     }
 
+    if(!bCin && !filename.size()) {
+      usage();
+      return -1;
+    }
 
-    while(!std::cin.eof())
-    {
-        script += std::cin.get();
+    if(bCin){
+      filename =  "<STDIN>";
+      read_script(std::cin,script);
+    }else{
+      std::ifstream file(filename.c_str());
+      if(file){
+	read_script(file,script);
+	file.close();
+      }else{
+	std::cerr<< "Couldn't open file: " << filename << std::endl;
+	return -1;
+      }
     }
 
     try{
-      AstScript * pScript = interpreter.prebuildAst("<STDIN>",script,bDebug,bScanDebug);
+      AstScript * pScript = interpreter.prebuildAst(filename,script,bDebug,bScanDebug);
 
         if(bPrint)
             pScript->print(std::cout);
