4272f94db7d7df586f4fe681578e7b36d6b7f67a
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2011-05-05 02:46:36 +0000

    Main menu targeting

---
+++ trunk/sr2/src/states/MainMenuTargetingState.cpp
@@ -0,0 +1,108 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "MainMenuTargetingState.h"
+#include "MainMenuState.h"
+
+using StoneRing::MainMenuTargetingState;
+
+
+MainMenuTargetingState::MainMenuTargetingState(MainMenuState& parent):m_parent(parent),m_bDone(false)
+{
+}
+MainMenuTargetingState::~MainMenuTargetingState()
+{
+}
+
+void MainMenuTargetingState::Init(bool PartyMode)
+{
+    m_parent.SelectionStart();
+    m_bPartyMode = PartyMode;
+    if(PartyMode){
+	m_parent.SelectAllCharacters();
+    }
+}
+
+
+bool MainMenuTargetingState::IsDone() const
+{
+    return m_bDone;
+}
+    // Handle joystick / key events that are processed according to mappings
+void MainMenuTargetingState::HandleButtonUp(const IApplication::Button& button)
+{
+    if(button == IApplication::BUTTON_CONFIRM)
+    {
+	m_parent.SelectionFinish();
+	m_bDone = true;
+    }
+    else if(button == IApplication::BUTTON_CANCEL)
+    {
+	m_parent.SelectionCancel();
+	m_bDone = true;
+    }
+}
+void MainMenuTargetingState::HandleAxisMove(const IApplication::Axis& axis, IApplication::AxisDirection dir, float pos)
+{
+    if(m_bPartyMode) return; 
+    
+    if(dir == IApplication::AXIS_UP)
+    {
+	m_parent.SelectCharacterUp();
+    }
+    else if(dir == IApplication::AXIS_DOWN)
+    {
+	m_parent.SelectCharacterDown();
+    }
+}
+    
+void MainMenuTargetingState::Draw(const CL_Rect &screenRect,CL_GraphicContext& GC)
+{
+}
+
+bool MainMenuTargetingState::LastToDraw() const
+{
+}
+
+bool MainMenuTargetingState::DisableMappableObjects() const
+{
+
+}
+
+void MainMenuTargetingState::MappableObjectMoveHook()
+{
+}
+
+void MainMenuTargetingState::Start()
+{
+    m_bDone = false;
+}
+
+void MainMenuTargetingState::SteelInit      (SteelInterpreter *)
+{
+
+}
+
+void MainMenuTargetingState::SteelCleanup   (SteelInterpreter *)
+{
+}
+
+void MainMenuTargetingState::Finish()
+{
+}
---
+++ trunk/sr2/src/states/MainMenuTargetingState.h
@@ -0,0 +1,56 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef MAINMENUTARGETINGSTATE_H
+#define MAINMENUTARGETINGSTATE_H
+
+#include "State.h"
+
+namespace StoneRing{
+
+class MainMenuState;
+
+class MainMenuTargetingState : public State
+{
+public:
+    MainMenuTargetingState(MainMenuState& parent);
+    virtual ~MainMenuTargetingState();
+    
+    void Init(bool PartyMode);
+    virtual bool IsDone() const;
+    // Handle joystick / key events that are processed according to mappings
+    virtual void HandleButtonUp(const IApplication::Button& button);
+    virtual void HandleAxisMove(const IApplication::Axis& axis, IApplication::AxisDirection dir, float pos);
+    
+    virtual void Draw(const CL_Rect &screenRect,CL_GraphicContext& GC);
+    virtual bool LastToDraw() const; // Should we continue drawing more states?
+    virtual bool DisableMappableObjects() const; // Should the app move the MOs?
+    virtual void MappableObjectMoveHook(); // Do stuff right after the mappable object movement
+    virtual void Start();
+    virtual void SteelInit      (SteelInterpreter *);
+    virtual void SteelCleanup   (SteelInterpreter *);
+    virtual void Finish(); // Hook to clean up or whatever after being popped
+private:
+    MainMenuState& m_parent;
+    bool m_bPartyMode;
+    bool m_bDone;
+};
+
+}
+#endif // MAINMENUTARGETING_H
