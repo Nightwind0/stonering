b42a404696913d623a789a364dc11ab8bb825f7b
nightwind <nightwind@1a319a91-200f-0410-9ca8-f36a151797b4>
2014-03-29 23:03:36 +0000

    Mac project

---
+++ sr2/MacProject/StoneRing2Cocoa/StoneRing2Cocoa/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  StoneRing2Cocoa
+//
+//  Created by Daniel Palm on 3/11/14.
+//  Copyright (c) 2014 Daniel Palm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
